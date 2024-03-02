0b0af9d4ce1e38706c2646b8a82cb2bc84de10af
Konstantin <k.a.golovnya@gmail.com>
2024-03-02 14:20:22 +1000

    Created ActivityDetailViewController

---
+++ ActivitiesNM/View/ActivityDetailHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ActivityDetailHeaderView.swift
+//  ActivitiesNM
+//
+//  Created by Konstantin on 01.03.2024.
+//
+
+import UIKit
+
+class ActivityDetailHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ActivitiesNM/View/ActivityDetailTextCell.swift
@@ -0,0 +1,23 @@
+//
+//  ActivityDetailTextCell.swift
+//  ActivitiesNM
+//
+//  Created by Konstantin on 02.03.2024.
+//
+
+import UIKit
+
+class ActivityDetailTextCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
